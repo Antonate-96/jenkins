22700dde90452a6aca390c905198df2070e74e24
natthaphon.a <natthaphon.a@takeitsolution.com>
2024-10-09 14:13:06 +0700

    Add credential

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
   title = 'Natthaphon';
   example = {
     "systemId": "test",
+    "password": "Mypas1234",
     "userName": "test001",
     "requestData": {
       "OFFCODE": "100400",
